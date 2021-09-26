d96abf06f5d80ef58e6af46afa85ce7569145d46
余乐旋 <y1339320456@outlook.com>
2021-09-26 18:04:14 +0800

    测试能不能上传到github

---
+++ HelloGit/src/main/java/com/atguigu/git/test_git/hell.java
@@ -0,0 +1,14 @@
+package com.atguigu.git.test_git;/* 
+hell - the name of the new file which you specify in the New File dialog box during the file creation. 
+余乐旋 - the login name of the current user. 
+2021/9/26 - the current system date. 
+18:03 - the current system time.  
+IntelliJ IDEA - the name of the IDE in which the file will be created. */
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class hell {
+    public static void main(String[] args) {
+        System.out.println("测试能不能更新项目");
+    }
+}
