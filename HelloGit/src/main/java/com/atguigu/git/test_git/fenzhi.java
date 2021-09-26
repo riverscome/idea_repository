2129456b2f3b5bec4dd059cb8245058c3eea21b1
余乐旋 <y1339320456@outlook.com>
2021-09-26 18:18:27 +0800

    测试分支

--- HelloGit/src/main/java/com/atguigu/git/test_git/fenzhi.java
+++ HelloGit/src/main/java/com/atguigu/git/test_git/fenzhi.java
@@ -1,16 +1,12 @@
 package com.atguigu.git.test_git;/* 
-hell - the name of the new file which you specify in the New File dialog box during the file creation. 
+fenzhi - the name of the new file which you specify in the New File dialog box during the file creation. 
 余乐旋 - the login name of the current user. 
 2021/9/26 - the current system date. 
-18:03 - the current system time.  
+18:13 - the current system time.  
 IntelliJ IDEA - the name of the IDE in which the file will be created. */
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-public class hell {
+public class fenzhi {
     public static void main(String[] args) {
-        System.out.println("测试能不能更新项目");
         System.out.println("创建分支");
-
     }
 }
