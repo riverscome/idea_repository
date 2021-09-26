31a086a3ee81fb6e5c08e9a790c3a78226514454
余乐旋 <y1339320456@outlook.com>
2021-09-26 18:01:49 +0800

    测试能不能上传到github

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;/* 
+HelloGit - the name of the new file which you specify in the New File dialog box during the file creation. 
+余乐旋 - the login name of the current user. 
+2021/9/26 - the current system date. 
+17:31 - the current system time.
+IntelliJ IDEA - the name of the IDE in which the file will be created. */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
